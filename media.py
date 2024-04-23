dabb95029eb6562b4637e169359e3918b13af9ac
Giani Augusto Braga <giani.braga80@gmail.com>
2024-04-23 19:54:23 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,8 @@
+# Exercicio para resolver média de notas
+
+nota_1 = float(input("Digite a nota 1"))
+nota_2 = float(input("Digite a nota 2"))
+
+media = (nota_1 + nota_2)/ 2
+
+print(" A nota final é: ", media)
